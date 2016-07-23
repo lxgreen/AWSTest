7d52be7cab39f25660d0d565509bcd6872d5169f
lxgreen <lxgreen@X3>
2016-07-23 16:17:43 +0300

    init commit

---
+++ AWSTestApp/AWSHelper.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.IdentityManagement;
+using Amazon.IdentityManagement.Model;
+using Amazon.Runtime;
+
+namespace AWSTestApp
+{
+    public class AWSHelper
+    {
+        public static IEnumerable<User> ListIamUsers()
+        {
+            var users = new List<User>();
+            var credentials = new StoredProfileAWSCredentials("dna_user_1");
+
+            using (var iamServiceClient = new AmazonIdentityManagementServiceClient(credentials))
+            {
+                var usersRequest = new ListUsersRequest();
+                var usersResponse = iamServiceClient.ListUsers(usersRequest);
+                users.AddRange(usersResponse.Users);
+
+                while (usersResponse.IsTruncated)
+                {
+                    usersRequest.Marker = usersResponse.Marker;
+                    usersResponse = iamServiceClient.ListUsers(usersRequest);
+                    users.AddRange(usersResponse.Users);
+                }
+            }
+            return users;
+        }
+
+        public static IEnumerable<AccessKeyMetadata> ListUserAccessKeys(User user)
+        {
+            var keys = new List<AccessKeyMetadata>();
+            var credentials = new StoredProfileAWSCredentials("dna_user_1");
+
+            using (var iamServiceClient = new AmazonIdentityManagementServiceClient(credentials))
+            {
+                var request = new ListAccessKeysRequest { UserName = user.UserName };
+                var response = iamServiceClient.ListAccessKeys(request);
+                keys.AddRange(response.AccessKeyMetadata);
+
+                while (response.IsTruncated)
+                {
+                    request.Marker = response.Marker;
+                    response = iamServiceClient.ListAccessKeys(request);
+                    keys.AddRange(response.AccessKeyMetadata);
+                }
+            }
+            return keys;
+        }
+
+        public static IEnumerable<Group> ListGroups()
+        {
+            var keys = new List<Group>();
+            var credentials = new StoredProfileAWSCredentials("dna_user_1");
+
+            using (var iamServiceClient = new AmazonIdentityManagementServiceClient(credentials))
+            {
+                var request = new ListGroupsRequest();
+                var response = iamServiceClient.ListGroups(request);
+                keys.AddRange(response.Groups);
+
+                while (response.IsTruncated)
+                {
+                    request.Marker = response.Marker;
+                    response = iamServiceClient.ListGroups(request);
+                    keys.AddRange(response.Groups);
+                }
+            }
+            return keys;
+        }
+
+        public static IEnumerable<Instance> ListEc2Instances()
+        {
+            var instances = new List<Instance>();
+            var credentials = new StoredProfileAWSCredentials("dna_user_1");
+            using (var client = new AmazonEC2Client(credentials, RegionEndpoint.USWest2))
+            {
+                var request = new DescribeInstancesRequest();
+                var response = client.DescribeInstances(request);
+                response.Reservations.ForEach(r => instances.AddRange(r.Instances));
+            }
+            return instances;
+        }
+
+        public static IEnumerable<KeyPairInfo> ListEc2KeyPairsByInstance(Instance instance)
+        {
+            var credentials = new StoredProfileAWSCredentials("dna_user_1");
+            using (var client = new AmazonEC2Client(credentials, RegionEndpoint.USWest2))
+            {
+                var request = new DescribeKeyPairsRequest() { KeyNames = new List<string>(new[] { instance.KeyName }) };
+                var response = client.DescribeKeyPairs(request);
+                return response.KeyPairs;
+            }
+        }
+    }
+}
---
+++ AWSTestApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using Amazon.EC2.Model;
+using Amazon.IdentityManagement.Model;
+
+namespace AWSTestApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("IAM Users:");
+            ListIamUsers();
+            ListGroups();
+            ListEc2Instances();
+
+            Console.ReadLine();
+        }
+
+        private static void ListGroups()
+        {
+            Console.WriteLine("IAM Group data:");
+            var groups = AWSHelper.ListGroups();
+            foreach (var group in groups)
+            {
+                Console.WriteLine($"{group.Arn} | {group.CreateDate} | {group.GroupId} | {group.GroupName} | {group.Path}");
+            }
+        }
+
+        private static void ListEc2KeyPairsByInstance(Instance instance)
+        {
+            Console.WriteLine($"EC2 instance {instance.InstanceId} keys data:");
+
+            var keyPairs = AWSHelper.ListEc2KeyPairsByInstance(instance);
+            foreach (var keyPair in keyPairs)
+            {
+                Console.WriteLine($"{keyPair.KeyName} | {keyPair.KeyFingerprint}");
+            }
+        }
+
+        private static void ListEc2Instances()
+        {
+            Console.WriteLine("EC2 instance data:");
+            var instances = AWSHelper.ListEc2Instances();
+            foreach (var i in instances)
+            {
+                Console.WriteLine($"{i.Architecture} | {i.ImageId} | {i.InstanceId} " +
+                    $"| {i.InstanceType} | {i.KernelId} | {i.KeyName} " +
+                    $"| {i.LaunchTime} | {i.Platform} | {i.PrivateIpAddress} | {i.PublicIpAddress}");
+                ListEc2KeyPairsByInstance(i);
+            }
+        }
+
+        private static void ListIamUserAccessKeys(User user)
+        {
+            var keys = AWSHelper.ListUserAccessKeys(user);
+            foreach (var key in keys)
+            {
+                Console.WriteLine($"{key.UserName} | {key.AccessKeyId} | {key.Status} | {key.CreateDate}");
+            }
+        }
+
+        private static void ListIamUsers()
+        {
+            var users = AWSHelper.ListIamUsers().ToList();
+            foreach (var user in users)
+            {
+                Console.WriteLine(
+                    $@"{user.UserName} | {user.Arn} | {user.CreateDate} | {user.PasswordLastUsed} |" +
+                    $@" {user.Path} | {user.UserId}");
+                Console.WriteLine($"{user.UserName} access key data:");
+                ListIamUserAccessKeys(user);
+            }
+        }
+    }
+}
